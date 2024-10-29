8425fb12c42a074805a2982cecbbfbdc54d8d92c
Iraida <iraidagonzalez.70@gmail.com>
2024-10-29 11:55:40 -0300

    primer commit de proyecto integrador

---
+++ main.py
@@ -0,0 +1,27 @@
+#MENU GESTION DE PRODUCTOS\n
+
+
+print("Menu de opciones\n")
+
+
+print("1. Alta de productos nuevos")
+print("2. Consulta de datos de productos")
+print("3. Modificar la  cantidad en stock de un producto")
+print("4. Dar de baja productos")
+print("5. Listado completo de productos")
+print("6. Lista de productos con cantidad bajo minimo")
+print("7. Salir")
+
+# PARA SOLICITAR AL USUARIO QUE SELECCIONE UNA OPCION,  CREO LA VARIABLE opcion
+
+opcion = int(input ("Introduzca una opcion (1-7):")  )
+
+#MOSTRAMOS LA OPCION SELECCIONADA
+
+print("Usted ha seleccionado:", opcion)
+
+
+
+
+
+
