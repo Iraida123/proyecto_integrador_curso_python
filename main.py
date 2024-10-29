8d38560135db3a31eb3deb4c5cb52e4b795aa247
Iraida <iraidagonzalez.70@gmail.com>
2024-10-29 15:22:20 -0300

    se agregaron cambios al menu

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 #MENU GESTION DE PRODUCTOS\n
+opcion = 0
+while opcion != 7:
+    print("Menu de opciones\n")
+    print("1. Alta de productos nuevos")
+    print("2. Consulta de datos de productos")
+    print("3. Modificar la  cantidad en stock de un producto")
+    print("4. Dar de baja productos")
+    print("5. Listado completo de productos")
+    print("6. Lista de productos con cantidad bajo minimo")
+    print("7. Salir")
 
-
-print("Menu de opciones\n")
-
-
-print("1. Alta de productos nuevos")
-print("2. Consulta de datos de productos")
-print("3. Modificar la  cantidad en stock de un producto")
-print("4. Dar de baja productos")
-print("5. Listado completo de productos")
-print("6. Lista de productos con cantidad bajo minimo")
-print("7. Salir")
-
-# PARA SOLICITAR AL USUARIO QUE SELECCIONE UNA OPCION,  CREO LA VARIABLE opcion
-
-opcion = int(input ("Introduzca una opcion (1-7):")  )
+    # PARA SOLICITAR AL USUARIO QUE SELECCIONE UNA OPCION,  CREO LA VARIABLE opcion
+    opcion = int(input("Introduzca una opcion (1-7):"))
+    if opcion == 7:                                                             
+       print("7. Salir")   
 
 #MOSTRAMOS LA OPCION SELECCIONADA
 
-print("Usted ha seleccionado:", opcion)
+
+     #print("Usted ha seleccionado:", opcion)
+
+
+
+
+    
+
+
 
 
 
